2997cd41f42c2390ec2a374b92396d9e60ea533f
Fellasfaw <fellasfaw@outlook.com>
2022-12-27 17:54:06 +0300

    - Modified Art and Painting classes - Added basic layout including buttons for the consumer’s experience - Added a picture box and a button for displaying the artwork - Created dummy objects for displaying on the UI

---
+++ ConsumerExperience.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ArtRepositorySystem
+{
+    partial class ConsumerExperience
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConsumerExperience));
+            this.PanelNavigation = new System.Windows.Forms.Panel();
+            this.PanelContent = new System.Windows.Forms.Panel();
+            this.BtnMyPlaylist = new System.Windows.Forms.Button();
+            this.BtnTopArtworks = new System.Windows.Forms.Button();
+            this.BtnTopArtists = new System.Windows.Forms.Button();
+            this.BtnMyFeed = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnSearch = new System.Windows.Forms.Button();
+            this.BtnNavigation = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PanelContent.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PanelNavigation
+            // 
+            this.PanelNavigation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.PanelNavigation.AutoSize = true;
+            this.PanelNavigation.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PanelNavigation.Location = new System.Drawing.Point(0, 0);
+            this.PanelNavigation.Name = "PanelNavigation";
+            this.PanelNavigation.Size = new System.Drawing.Size(245, 701);
+            this.PanelNavigation.TabIndex = 0;
+            // 
+            // PanelContent
+            // 
+            this.PanelContent.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PanelContent.AutoSize = true;
+            this.PanelContent.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PanelContent.Controls.Add(this.button1);
+            this.PanelContent.Controls.Add(this.BtnMyPlaylist);
+            this.PanelContent.Controls.Add(this.BtnTopArtworks);
+            this.PanelContent.Controls.Add(this.BtnTopArtists);
+            this.PanelContent.Controls.Add(this.BtnMyFeed);
+            this.PanelContent.Controls.Add(this.pictureBox1);
+            this.PanelContent.Controls.Add(this.BtnSearch);
+            this.PanelContent.Controls.Add(this.BtnNavigation);
+            this.PanelContent.Location = new System.Drawing.Point(251, 0);
+            this.PanelContent.Name = "PanelContent";
+            this.PanelContent.Size = new System.Drawing.Size(946, 701);
+            this.PanelContent.TabIndex = 1;
+            // 
+            // BtnMyPlaylist
+            // 
+            this.BtnMyPlaylist.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.BtnMyPlaylist.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnMyPlaylist.Location = new System.Drawing.Point(646, 67);
+            this.BtnMyPlaylist.Name = "BtnMyPlaylist";
+            this.BtnMyPlaylist.Size = new System.Drawing.Size(172, 47);
+            this.BtnMyPlaylist.TabIndex = 6;
+            this.BtnMyPlaylist.Text = "My Playlist";
+            this.BtnMyPlaylist.UseVisualStyleBackColor = true;
+            // 
+            // BtnTopArtworks
+            // 
+            this.BtnTopArtworks.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.BtnTopArtworks.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnTopArtworks.Location = new System.Drawing.Point(468, 67);
+            this.BtnTopArtworks.Name = "BtnTopArtworks";
+            this.BtnTopArtworks.Size = new System.Drawing.Size(172, 47);
+            this.BtnTopArtworks.TabIndex = 5;
+            this.BtnTopArtworks.Text = "Top Artworks";
+            this.BtnTopArtworks.UseVisualStyleBackColor = true;
+            // 
+            // BtnTopArtists
+            // 
+            this.BtnTopArtists.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.BtnTopArtists.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnTopArtists.Location = new System.Drawing.Point(290, 67);
+            this.BtnTopArtists.Name = "BtnTopArtists";
+            this.BtnTopArtists.Size = new System.Drawing.Size(172, 47);
+            this.BtnTopArtists.TabIndex = 4;
+            this.BtnTopArtists.Text = "Top Artists";
+            this.BtnTopArtists.UseVisualStyleBackColor = true;
+            // 
+            // BtnMyFeed
+            // 
+            this.BtnMyFeed.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.BtnMyFeed.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnMyFeed.Location = new System.Drawing.Point(112, 67);
+            this.BtnMyFeed.Name = "BtnMyFeed";
+            this.BtnMyFeed.Size = new System.Drawing.Size(172, 47);
+            this.BtnMyFeed.TabIndex = 3;
+            this.BtnMyFeed.Text = "My Feed";
+            this.BtnMyFeed.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(112, 176);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(10);
+            this.pictureBox1.Size = new System.Drawing.Size(301, 200);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // BtnSearch
+            // 
+            this.BtnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnSearch.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BtnSearch.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnSearch.BackgroundImage = global::ArtRepositorySystem.Properties.Resources.loupe;
+            this.BtnSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnSearch.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnSearch.Location = new System.Drawing.Point(893, 12);
+            this.BtnSearch.Name = "BtnSearch";
+            this.BtnSearch.Size = new System.Drawing.Size(50, 50);
+            this.BtnSearch.TabIndex = 1;
+            this.BtnSearch.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.BtnSearch.UseVisualStyleBackColor = false;
+            // 
+            // BtnNavigation
+            // 
+            this.BtnNavigation.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BtnNavigation.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnNavigation.BackgroundImage = global::ArtRepositorySystem.Properties.Resources.menu_bar;
+            this.BtnNavigation.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnNavigation.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnNavigation.Location = new System.Drawing.Point(12, 12);
+            this.BtnNavigation.Name = "BtnNavigation";
+            this.BtnNavigation.Size = new System.Drawing.Size(50, 50);
+            this.BtnNavigation.TabIndex = 0;
+            this.BtnNavigation.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.BtnNavigation.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(428, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(301, 200);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "X";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ConsumerExperience
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1197, 701);
+            this.Controls.Add(this.PanelNavigation);
+            this.Controls.Add(this.PanelContent);
+            this.Name = "ConsumerExperience";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ArtRepositorySystem";
+            this.PanelContent.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Panel PanelNavigation;
+        private Panel PanelContent;
+        private Button BtnSearch;
+        private Button BtnNavigation;
+        private PictureBox pictureBox1;
+        private Button BtnMyPlaylist;
+        private Button BtnTopArtworks;
+        private Button BtnTopArtists;
+        private Button BtnMyFeed;
+        private Button button1;
+    }
+}
---
+++ ConsumerExperience.cs
@@ -0,0 +1,52 @@
+namespace ArtRepositorySystem
+{
+    public partial class ConsumerExperience : Form
+    {
+        public ConsumerExperience()
+        {
+            InitializeComponent();
+            InitializeDummyObjects();
+        }
+
+        public void InitializeDummyObjects()
+        {
+            Painting p = new Painting();
+            p.Title = "The Total Liberation of Africa";
+            p.Image = Image.FromFile("C:\\Users\\zelal\\source\\repos\\FinhasYohannesGustavo\\ArtRepositorySystem\\Resources\\The Total Liberation of Africa.jpg");
+            p.Description = @"
+                In 1958, Ethiopian multi-disciplinary artist Afewerk Tekle created arguably his greatest work: a 
+                stained-glass triptych entitled 'The Total Liberation of Africa', commissioned to be installed in 
+                the newly established Africa Hall, the headquarters of the United Nations Economic Commission for 
+                Africa, in Addis Ababa.
+            ";
+            p.Shape = PaintingShape.Rectangle;
+
+            User afewerk = new User();
+            afewerk.Username = "afewerk_tekle";
+            afewerk.FirstName = "Afewerk";
+            afewerk.LastName = "Tekle";
+            afewerk.Bio = "The Shit.";
+            afewerk.Works = new List<Art>();
+            afewerk.Works.Add(p);
+
+            p.Artists = new List<User> {afewerk};
+
+            pictureBox1.BackgroundImage = p.Image;
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //Get an Art object based on the picture box that was clicked and make sure all its properties are filled
+            //Create a new panel that hovers above everything
+            //This panel is a card layout panel that has 4 cards {art, description & artist, feedback, analytic}
+                // art
+                // description & artist
+                // feedback form
+                // analytics
+            //Buttons appear to the right and left of the panel for navigating through the cards
+                // The button on the left is disabled when on page 1
+                // The button on the right is disabled when on page 4
+            
+        }
+    }
+}
