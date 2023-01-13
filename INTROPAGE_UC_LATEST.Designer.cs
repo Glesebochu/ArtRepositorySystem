04b27f30a1ed15ca19a27e4d29d0987e0f69a7e0
Naod816 <naodteen816@gmail.com>
2023-01-13 11:00:43 +0300

    ADD ALL NECCESSARY USERCONTROL FILES

---
+++ INTROPAGE_UC_LATEST.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace IP_UC_Latest
+{
+    partial class INTROPAGE_UC_LATEST
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Panel7 = new Guna.UI2.WinForms.Guna2Panel();
+            this.Panel6 = new Guna.UI2.WinForms.Guna2Panel();
+            this.Panel5 = new Guna.UI2.WinForms.Guna2Panel();
+            this.Panel4 = new Guna.UI2.WinForms.Guna2Panel();
+            this.Panel3 = new Guna.UI2.WinForms.Guna2Panel();
+            this.Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.PictureBox2 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.PictureBox3 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.PictureBox4 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.PictureBox5 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.PictureBox6 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.PictureBox7 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.Panel7.SuspendLayout();
+            this.Panel6.SuspendLayout();
+            this.Panel5.SuspendLayout();
+            this.Panel4.SuspendLayout();
+            this.Panel3.SuspendLayout();
+            this.Panel2.SuspendLayout();
+            this.Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Panel7
+            // 
+            this.Panel7.Controls.Add(this.Panel6);
+            this.Panel7.Controls.Add(this.PictureBox7);
+            this.Panel7.Location = new System.Drawing.Point(0, 0);
+            this.Panel7.Name = "Panel7";
+            this.Panel7.Size = new System.Drawing.Size(930, 528);
+            this.Panel7.TabIndex = 0;
+            // 
+            // Panel6
+            // 
+            this.Panel6.Controls.Add(this.Panel5);
+            this.Panel6.Controls.Add(this.PictureBox6);
+            this.Panel6.Location = new System.Drawing.Point(0, 0);
+            this.Panel6.Name = "Panel6";
+            this.Panel6.Size = new System.Drawing.Size(930, 528);
+            this.Panel6.TabIndex = 1;
+            // 
+            // Panel5
+            // 
+            this.Panel5.Controls.Add(this.Panel4);
+            this.Panel5.Controls.Add(this.PictureBox5);
+            this.Panel5.Location = new System.Drawing.Point(0, 0);
+            this.Panel5.Name = "Panel5";
+            this.Panel5.Size = new System.Drawing.Size(930, 528);
+            this.Panel5.TabIndex = 1;
+            // 
+            // Panel4
+            // 
+            this.Panel4.Controls.Add(this.Panel3);
+            this.Panel4.Controls.Add(this.PictureBox4);
+            this.Panel4.Location = new System.Drawing.Point(0, 0);
+            this.Panel4.Name = "Panel4";
+            this.Panel4.Size = new System.Drawing.Size(930, 528);
+            this.Panel4.TabIndex = 1;
+            // 
+            // Panel3
+            // 
+            this.Panel3.Controls.Add(this.Panel2);
+            this.Panel3.Controls.Add(this.PictureBox3);
+            this.Panel3.Location = new System.Drawing.Point(0, 0);
+            this.Panel3.Name = "Panel3";
+            this.Panel3.Size = new System.Drawing.Size(930, 528);
+            this.Panel3.TabIndex = 1;
+            // 
+            // Panel2
+            // 
+            this.Panel2.Controls.Add(this.Panel1);
+            this.Panel2.Controls.Add(this.PictureBox2);
+            this.Panel2.Location = new System.Drawing.Point(0, 0);
+            this.Panel2.Name = "Panel2";
+            this.Panel2.Size = new System.Drawing.Size(930, 528);
+            this.Panel2.TabIndex = 1;
+            // 
+            // Panel1
+            // 
+            this.Panel1.Controls.Add(this.PictureBox1);
+            this.Panel1.Location = new System.Drawing.Point(0, 0);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(930, 528);
+            this.Panel1.TabIndex = 1;
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Image = global::IP_UC_Latest.Properties.Resources._1;
+            this.PictureBox1.ImageRotate = 0F;
+            this.PictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(930, 528);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox1.TabIndex = 0;
+            this.PictureBox1.TabStop = false;
+            this.PictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // PictureBox2
+            // 
+            this.PictureBox2.Image = global::IP_UC_Latest.Properties.Resources._2;
+            this.PictureBox2.ImageRotate = 0F;
+            this.PictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.PictureBox2.Name = "PictureBox2";
+            this.PictureBox2.Size = new System.Drawing.Size(930, 528);
+            this.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox2.TabIndex = 0;
+            this.PictureBox2.TabStop = false;
+            // 
+            // PictureBox3
+            // 
+            this.PictureBox3.Image = global::IP_UC_Latest.Properties.Resources._3;
+            this.PictureBox3.ImageRotate = 0F;
+            this.PictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.PictureBox3.Name = "PictureBox3";
+            this.PictureBox3.Size = new System.Drawing.Size(930, 528);
+            this.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox3.TabIndex = 0;
+            this.PictureBox3.TabStop = false;
+            // 
+            // PictureBox4
+            // 
+            this.PictureBox4.Image = global::IP_UC_Latest.Properties.Resources._4;
+            this.PictureBox4.ImageRotate = 0F;
+            this.PictureBox4.Location = new System.Drawing.Point(0, 0);
+            this.PictureBox4.Name = "PictureBox4";
+            this.PictureBox4.Size = new System.Drawing.Size(930, 528);
+            this.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox4.TabIndex = 0;
+            this.PictureBox4.TabStop = false;
+            this.PictureBox4.Click += new System.EventHandler(this.PictureBox4_Click);
+            // 
+            // PictureBox5
+            // 
+            this.PictureBox5.Image = global::IP_UC_Latest.Properties.Resources._5;
+            this.PictureBox5.ImageRotate = 0F;
+            this.PictureBox5.Location = new System.Drawing.Point(0, 0);
+            this.PictureBox5.Name = "PictureBox5";
+            this.PictureBox5.Size = new System.Drawing.Size(930, 528);
+            this.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox5.TabIndex = 0;
+            this.PictureBox5.TabStop = false;
+            // 
+            // PictureBox6
+            // 
+            this.PictureBox6.Image = global::IP_UC_Latest.Properties.Resources._6;
+            this.PictureBox6.ImageRotate = 0F;
+            this.PictureBox6.Location = new System.Drawing.Point(0, 0);
+            this.PictureBox6.Name = "PictureBox6";
+            this.PictureBox6.Size = new System.Drawing.Size(930, 528);
+            this.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox6.TabIndex = 0;
+            this.PictureBox6.TabStop = false;
+            // 
+            // PictureBox7
+            // 
+            this.PictureBox7.Image = global::IP_UC_Latest.Properties.Resources._7;
+            this.PictureBox7.ImageRotate = 0F;
+            this.PictureBox7.Location = new System.Drawing.Point(0, 0);
+            this.PictureBox7.Name = "PictureBox7";
+            this.PictureBox7.Size = new System.Drawing.Size(930, 528);
+            this.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox7.TabIndex = 0;
+            this.PictureBox7.TabStop = false;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.White;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button1.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button1.Location = new System.Drawing.Point(53, 532);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(180, 45);
+            this.guna2Button1.TabIndex = 1;
+            this.guna2Button1.Text = "<< BACK";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.White;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button2.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button2.Location = new System.Drawing.Point(341, 532);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(180, 45);
+            this.guna2Button2.TabIndex = 2;
+            this.guna2Button2.Text = "NEXT >>";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // guna2Button3
+            // 
+            this.guna2Button3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button3.FillColor = System.Drawing.Color.White;
+            this.guna2Button3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button3.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button3.Location = new System.Drawing.Point(606, 532);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.Size = new System.Drawing.Size(281, 45);
+            this.guna2Button3.TabIndex = 3;
+            this.guna2Button3.Text = "SKIP TO REGISTRATION";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // INTROPAGE_UC_LATEST
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Controls.Add(this.guna2Button3);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.Panel7);
+            this.Name = "INTROPAGE_UC_LATEST";
+            this.Size = new System.Drawing.Size(930, 580);
+            this.Panel7.ResumeLayout(false);
+            this.Panel6.ResumeLayout(false);
+            this.Panel5.ResumeLayout(false);
+            this.Panel4.ResumeLayout(false);
+            this.Panel3.ResumeLayout(false);
+            this.Panel2.ResumeLayout(false);
+            this.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox7)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel Panel7;
+        private Guna.UI2.WinForms.Guna2PictureBox PictureBox7;
+        private Guna.UI2.WinForms.Guna2Panel Panel6;
+        private Guna.UI2.WinForms.Guna2PictureBox PictureBox6;
+        private Guna.UI2.WinForms.Guna2Panel Panel5;
+        private Guna.UI2.WinForms.Guna2PictureBox PictureBox5;
+        private Guna.UI2.WinForms.Guna2Panel Panel4;
+        private Guna.UI2.WinForms.Guna2PictureBox PictureBox4;
+        private Guna.UI2.WinForms.Guna2Panel Panel3;
+        private Guna.UI2.WinForms.Guna2PictureBox PictureBox3;
+        private Guna.UI2.WinForms.Guna2Panel Panel2;
+        private Guna.UI2.WinForms.Guna2PictureBox PictureBox2;
+        private Guna.UI2.WinForms.Guna2Panel Panel1;
+        private Guna.UI2.WinForms.Guna2PictureBox PictureBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ INTROPAGE_UC_LATEST.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IP_UC_Latest
+{
+    public partial class INTROPAGE_UC_LATEST : UserControl
+    {
+        const int ShrinkSize = 10;
+        int PanelCount = 0;
+        public INTROPAGE_UC_LATEST()
+        {
+            InitializeComponent();
+            guna2Button1.Visible = false;
+        }
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+            PanelCount -= 1;
+        }
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            PanelCount += 1;
+
+            guna2Button1.Visible = true;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (PanelCount == 1)
+            {
+                Panel1.Width -= ShrinkSize;
+                if (Panel1.Width <= 0)
+                {
+                    timer1.Stop();
+                }
+            }
+            else if (PanelCount == 2)
+            {
+                Panel2.Width -= ShrinkSize;
+                if (Panel2.Width <= 0)
+                {
+                    timer1.Stop();
+                }
+            }
+            else if (PanelCount == 3)
+            {
+                Panel3.Width -= ShrinkSize;
+                if (Panel3.Width <= 0)
+                {
+                    timer1.Stop();
+                }
+            }
+            else if (PanelCount == 4)
+            {
+                Panel4.Width -= ShrinkSize;
+                if (Panel4.Width <= 0)
+                {
+                    timer1.Stop();
+                }
+            }
+            else if (PanelCount == 5)
+            {
+                Panel5.Width -= ShrinkSize;
+                if (Panel5.Width <= 0)
+                {
+                    timer1.Stop();
+                }
+            }
+            else if (PanelCount == 6)
+            {
+                Panel6.Width -= ShrinkSize;
+                if (Panel6.Width <= 0)
+                {
+                    timer1.Stop();
+                }
+            }
+            else
+            {
+                Panel7.Width -= ShrinkSize;
+                if (Panel7.Width <= 0)
+                {
+                    timer1.Stop();
+                }
+            }
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (PanelCount == 6)
+            {
+                Panel7.Width += ShrinkSize;
+                if (Panel7.Width >= 930)
+                {
+                    timer2.Stop();
+                }
+            }
+            else if (PanelCount == 5)
+            {
+                Panel6.Width += ShrinkSize;
+                if (Panel6.Width >= 930)
+                {
+                    timer2.Stop();
+                }
+            }
+            else if (PanelCount == 4)
+            {
+                Panel5.Width += ShrinkSize;
+                if (Panel5.Width >= 930)
+                {
+                    timer2.Stop();
+                }
+            }
+            else if (PanelCount == 3)
+            {
+                Panel4.Width += ShrinkSize;
+                if (Panel4.Width >= 930)
+                {
+                    timer2.Stop();
+                }
+            }
+            else if (PanelCount == 2)
+            {
+                Panel3.Width += ShrinkSize;
+                if (Panel3.Width >= 930)
+                {
+                    timer2.Stop();
+                }
+            }
+            else if (PanelCount == 1)
+            {
+                Panel2.Width += ShrinkSize;
+                if (Panel2.Width >= 930)
+                {
+                    timer2.Stop();
+                }
+            }
+            else
+            {
+                Panel1.Width += ShrinkSize;
+                if (Panel1.Width >= 930)
+                {
+                    timer2.Stop();
+                }
+
+                guna2Button1.Visible = false;
+            }
+        }
+        private void PictureBox4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
