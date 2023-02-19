3bb3250ab0cd3031c7bd8859a3494a00540aa9fc
Abthon <walelignabenezer@gmail.com>
2023-02-19 01:59:39 -0800

    Add a Questions.cs file

---
+++ Questions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtRepositorySystem
+{
+    public class Questions
+    {
+        public static List<string> feedBackQuestions = new List<string>() { "1) Does your opinion about the artwork change the longer you pay attention to it? ", "2) Is the artwork engaging?","3) Is the art style compatible with the artwork's intent?", "4) Do you find the composition of the art intriguing? ", "5) What would elevate the artwork further?"};
+    }
+}
